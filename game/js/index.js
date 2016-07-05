cb9869e1bd8a3a526fe026bfd10c4bde02a82696
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-07-05 17:13:02 +0200

    Upload new version by Liliana

--- game/js/index.js
+++
@@ -1,9 +0,0 @@
-(function (win, doc) {
-  var terminal = doc.getElementById('terminal')
-
-  terminal.innerHTML = '$ '
-
-  win.addEventListener('keypress', function (e) {
-    terminal.innerHTML += e.key
-  })
-})(window, document)
